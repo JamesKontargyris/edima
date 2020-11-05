9671821861713fd4af508e2976329632803a82d5
James Kontargyris <james@jameskontargyris.co.uk>
2020-11-05 19:33:52 +0000

    Various minor tweaks ahead of DOT Europe re-launch

--- wp-content/themes/edima/template-parts/content-document-archive.php
+++ wp-content/themes/edima/template-parts/content-document-archive.php
@@ -1,20 +1,22 @@
-<div class="document-archive__page-header stripe stripe--large-padding margin--none">
-	<div class="stripe__content stripe__content--side-padding">
+<?php if ( get_theme_mod( 'document_banner_heading' ) || get_theme_mod( 'document_banner_text' ) ) : ?>
+    <div class="document-archive__page-header stripe stripe--large-padding margin--none">
+        <div class="stripe__content stripe__content--side-padding">
 
-		<div class="page-header__content">
+            <div class="page-header__content">
 
-			<?php if(get_theme_mod('document_banner_heading')) : ?>
-				<h1 class="page-header__title"><?php echo get_theme_mod('document_banner_heading', 'Library'); ?></h1>
-			<?php endif; ?>
+				<?php if ( get_theme_mod( 'document_banner_heading' ) ) : ?>
+                    <h1 class="page-header__title"><?php echo get_theme_mod( 'document_banner_heading', 'Library' ); ?></h1>
+				<?php endif; ?>
 
-			<?php if(get_theme_mod('document_banner_text')) : ?>
-				<p class="page-header__intro text--page-intro text--page-intro--centered"><?php echo get_theme_mod('document_banner_text'); ?></p>
-			<?php endif; ?>
+				<?php if ( get_theme_mod( 'document_banner_text' ) ) : ?>
+                    <p class="page-header__intro text--page-intro text--page-intro--centered"><?php echo get_theme_mod( 'document_banner_text' ); ?></p>
+				<?php endif; ?>
 
-		</div>
+            </div>
 
-	</div>
-</div>
+        </div>
+    </div>
+<?php endif; ?>
 
 <!--<div class="news__sub-nav page-nav page-nav--green page-nav--center margin--bottom-large">
     <div class="page-nav__content">
@@ -23,10 +25,10 @@
 </div>-->
 
 <!--Featured documents-->
-<?php get_template_part('template-parts/partials/partial', 'featured-documents'); ?>
+<?php get_template_part( 'template-parts/partials/partial', 'featured-documents' ); ?>
 <!--/Featured documents-->
 
 <!--Standard documents-->
-<?php get_template_part('template-parts/partials/partial', 'standard-documents'); ?>
+<?php get_template_part( 'template-parts/partials/partial', 'standard-documents' ); ?>
 <!--/Standard documents-->
 
